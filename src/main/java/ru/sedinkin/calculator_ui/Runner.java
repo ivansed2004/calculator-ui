647027d01f6508fc43027cd93d2fc49516409c22
ivan <id2like0to0know4@gmail.com>
2025-07-16 22:11:21 +0500

    renamed: in Runner.java: methods names has been changed: persisted -> discrete; printed -> analytical

---
+++ src/main/java/ru/sedinkin/calculator_ui/Runner.java
@@ -0,0 +1,159 @@
+package ru.sedinkin.calculator_ui;
+
+import ru.sedinkin.calculator_core.builders.HyperbolaExpressionBuilder;
+import ru.sedinkin.calculator_core.builders.InterferogramExpressionBuilder;
+import ru.sedinkin.calculator_core.builders.UnsignedInterferogramExpressionBuilder;
+import ru.sedinkin.calculator_core.objects.hyperbola.Hyperbola;
+import ru.sedinkin.calculator_core.objects.interferogram.Interferogram;
+import ru.sedinkin.calculator_core.objects.spline.Spline;
+import ru.sedinkin.calculator_core.utils.SplineEquationResolver;
+import ru.sedinkin.calculator_core.sample_printer.HyperbolaSamplePrinter;
+import ru.sedinkin.calculator_core.sample_printer.InterferogramSamplePrinter;
+import ru.sedinkin.calculator_core.sample_printer.SplineSamplePrinter;
+import ru.sedinkin.calculator_core.expression_printer.HyperbolaExpressionPrinter;
+import ru.sedinkin.calculator_core.expression_printer.InterferogramExpressionPrinter;
+import ru.sedinkin.calculator_core.samplers.HyperbolaSampler;
+import ru.sedinkin.calculator_core.samplers.InterferogramSampler;
+import ru.sedinkin.calculator_core.samplers.SplineSampler;
+import ru.sedinkin.calculator_ui.environment.YamlParser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static ru.sedinkin.calculator_ui.environment.Environment.*;
+
+public class Runner {
+
+    public static void main(String[] args) throws Exception {
+        YamlParser.setDefaultTargetDirectory();
+        YamlParser.parseRuntimeTargetDirectorySelection();
+        if ( Objects.equals(RUNTIME_TARGET_DIR_SELECTION, "always") ) {
+            selectTargetDirectory();
+        }
+        System.out.println(TARGET_DIRECTORY);
+        start( selectSourceFiles() );
+    }
+
+    private static void start( File[] filesToOpen ) throws IOException {
+        System.out.println("Start generating...");
+        for ( File file : filesToOpen ) {
+            int fileNum = Integer.parseInt( file.getName().split("\\.")[0] );
+            String targetPath = TARGET_DIRECTORY + SEP + String.format("%d", fileNum);
+
+            if ( Files.exists(Paths.get(targetPath)) ) {
+                continue;
+            }
+
+            String spectrumDiscreteFilename = String.format("spectrum_discrete%d.dat", fileNum);
+            String interferogramDiscreteFilename = String.format("interferogram_discrete%d.dat", fileNum);
+            String interferogramAnalyticalFilename = String.format("interferogram_analytical%d.txt", fileNum);
+            String hyperbolaDiscreteFilename = String.format("hyperbola_discrete%d.dat", fileNum);
+            String hyperbolaAnalyticalFilename = String.format("hyperbola_analytical%d.txt", fileNum);
+
+            Spline sbf = getSplineBasedFunction( file, 20 );
+            getPersistedSpectrum( sbf, targetPath, spectrumDiscreteFilename );
+
+            Interferogram interferogram = getInterferogram( sbf );
+            getPersistedInterferogram( interferogram, targetPath, interferogramDiscreteFilename );
+            getPrintedInterferogram( interferogram, targetPath, interferogramAnalyticalFilename );
+
+            Hyperbola hyperbola = getHyperbola( interferogram );
+            getPersistedHyperbola( hyperbola, targetPath, hyperbolaDiscreteFilename );
+            getPrintedHyperbola( hyperbola, targetPath, hyperbolaAnalyticalFilename );
+
+            System.out.printf("\nThe directory for source file №%d has been generated.\n", fileNum);
+        }
+    }
+
+    private static File[] selectSourceFiles() {
+        Frame frame = new Frame("Source files issue");
+        FileDialog fileDialog = new FileDialog( frame, "Select source files", FileDialog.LOAD );
+        fileDialog.setMultipleMode(true);
+        fileDialog.setVisible(true);
+        File[] filesToOpen = fileDialog.getFiles();
+        frame.dispose();
+        return filesToOpen;
+    }
+
+    private static void selectTargetDirectory() throws Exception {
+        setSystemLookAndFeel();
+
+        Frame frame = new Frame("Source files issue");
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        int userSelection = fileChooser.showOpenDialog(frame);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            TARGET_DIRECTORY = fileChooser.getSelectedFile().getAbsolutePath();
+        }
+        frame.dispose();
+    }
+
+    public static void getPrintedHyperbola( Hyperbola hyperbola, String targetPath, String fileName )
+            throws IOException {
+        HyperbolaExpressionBuilder builder = new HyperbolaExpressionBuilder();
+        HyperbolaExpressionPrinter printer = new HyperbolaExpressionPrinter();
+
+        List<String> stringsToPrint = builder.perform( hyperbola, Map.of() );
+        File file = printer.perform( stringsToPrint, Map.of() );
+    }
+
+    public static void getPersistedHyperbola( Hyperbola hyperbola, String targetPath, String fileName )
+            throws IOException {
+        HyperbolaSampler sampler = new HyperbolaSampler();
+        HyperbolaSamplePrinter printer = new HyperbolaSamplePrinter();
+
+        Map<Double, Double> samples = sampler
+                .perform( hyperbola, Map.of("start", 1429.155, "end", 4000.092, "period", 1.929)  );
+        File file = printer.perform( samples, Map.of() );
+    }
+
+    public static Hyperbola getHyperbola( Interferogram interferogram ) {
+        return new Hyperbola( interferogram );
+    }
+
+    public static void getPrintedInterferogram( Interferogram interferogram, String targetPath, String fileName )
+            throws IOException {
+        InterferogramExpressionBuilder builder = new UnsignedInterferogramExpressionBuilder();
+        InterferogramExpressionPrinter printer = new InterferogramExpressionPrinter();
+
+        List<String> stringsToPrint = builder.perform( interferogram, Map.of() );
+        File file = printer.perform( stringsToPrint, Map.of() );
+    }
+
+    public static void getPersistedInterferogram( Interferogram interferogram, String targetPath, String fileName )
+            throws IOException {
+        InterferogramSampler sampler = new InterferogramSampler();
+        InterferogramSamplePrinter printer = new InterferogramSamplePrinter();
+
+        Map<Double, Double> samples = sampler
+                .perform( interferogram, Map.of("start", 1429.155, "end", 4000.092, "period", 1.929) );
+        File file = printer.perform( samples, Map.of() );
+    }
+
+    public static Interferogram getInterferogram( Spline sbf ) {
+        return new Interferogram( sbf );
+    }
+
+    public static void getPersistedSpectrum( Spline sbf, String targetPath, String fileName )
+            throws IOException {
+        SplineSampler sampler = new SplineSampler();
+        SplineSamplePrinter printer = new SplineSamplePrinter();
+
+        Map<Double, Double> samples = sampler
+                .perform( sbf, Map.of( "period", 1.929 ) );
+        File file = printer.perform( samples, Map.of() );
+    }
+
+    public static Spline getSplineBasedFunction( File file, int period ) {
+        return SplineEquationResolver.resolve( file, period );
+    }
+
+}
